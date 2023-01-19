bc0c11ecb0fb9e4459113e5b10c2366c1a7518ba
Shashank <mishra.shashankofficial06@gmail.com>
2023-01-19 16:09:00 +0530

    Added : Calculate employee monthly wage

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -4,8 +4,6 @@ console.log("Welcome to Employee Wage Computation Program");
  * 1) initializing a const variable
  * 2) using Math.floor() to return the greatest integer or equal to the given number & Math.random() to
  * return the number in the range of [0,1]
- * 3) using if-else conditional statement, if empAttendance == isAbsent then the employee is absent else
- * employee is present
 */
 
 // 1) initializing a const variable
@@ -15,20 +13,7 @@ const isAbsent = 0;
  * 2) using Math.floor() to return the greatest integer or equal to the given number & Math.random() to
    return the number in the range of [0,1]
  */
-let empAttendence = Math.floor(Math.random() * 10) % 3;
-
-/** 
- * 3) using if-else conditional statement, if empAttendance == isAbsent then the employee is absent else
-   employee is present
-*/
-if (empAttendence == isAbsent) {
-    console.log("Employee is Absent.");
-    return;
-} else if(empAttendence == 1) {
-    console.log("Employee is Present.");
-}else {
-    console.log("Employee is present half day.");
-}
+// let empAttendence = Math.floor(Math.random() * 10) % 3;
 /**
  * 1) declaring const variable
  * 2) using switch case to calculate daily employee wage
@@ -39,20 +24,32 @@ const WAGE_PER_HOUR = 20;
 const FULL_DAY_WORKING_HOUR = 8;
 const PART_TIME_WORKING_HOUR = 4;
 let empHours = 0;
+let monthlySalary = 0;
+let empWage = 0;
+let workingDayPerMonth = 20;
 
 // 2) using switch case to calculate daily employee wage
-switch(empAttendence){
-    case 0:
-        empHours = 0;
-    case 1:
-        empHours = FULL_DAY_WORKING_HOUR;
-        empWage = WAGE_PER_HOUR * FULL_DAY_WORKING_HOUR;
-        break;
-    case 2:
-        empHours = PART_TIME_WORKING_HOUR;
-        empWage = WAGE_PER_HOUR * PART_TIME_WORKING_HOUR;
-        break;   
+var i;
+for (i = 1; i <= workingDayPerMonth; i++){
+    switch(Math.floor(Math.random() * 10) % 3){
+        case 0:
+            console.log("Employee is Absent.");
+            empHours = 0;
+            break;
+        case 1:
+            console.log("Employee is Present.");
+            empHours = FULL_DAY_WORKING_HOUR;
+            empWage = WAGE_PER_HOUR * FULL_DAY_WORKING_HOUR;
+            break;
+        case 2:
+            console.log("Employee is present half day.");
+            empHours = PART_TIME_WORKING_HOUR;
+            empWage = WAGE_PER_HOUR * PART_TIME_WORKING_HOUR;
+            break;   
+    }
+    monthlySalary += empWage;
 }
 
+
 // let empWage = empHours * WAGE_PER_HOUR;
-console.log("Employee wage is: "+ empWage);
+console.log("Employee monthly wage is: "+ monthlySalary);
