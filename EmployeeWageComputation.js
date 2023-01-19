609ba8bd0f37f6b4a1769cf38cb4c6cb8802123d
Shashank <mishra.shashankofficial06@gmail.com>
2023-01-19 15:25:35 +0530

    Added : Calculate daily employee wage

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -28,3 +28,28 @@ if (empAttendence == isAbsent) {
 } else {
     console.log("Employee is Present.");
 }
+
+// UC-2 Calculate Daily employee wage
+/**
+ * 1) declaring const variable
+ * 2) using switch case to calculate daily employee wage
+ */
+
+// 1) declaring const variable
+const WAGE_PER_HOUR = 20;
+const FULL_DAY_WORKING_HOUR = 8;
+let empHours = 0;
+
+let empCheck = Math.floor(Math.random() * 10) % 2;
+
+// 2) using switch case to calculate daily employee wage
+switch(empCheck){
+    case 0:
+        empHours = 0;
+    case 1:
+        empHours = FULL_DAY_WORKING_HOUR;
+        break;
+}
+
+let empWage = empHours * WAGE_PER_HOUR;
+console.log("Daily Employee wage is: "+ empWage);
